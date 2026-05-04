535fc004fe4d8badb1092a5fabf74c5f8a076c4a
Agus <cuentauniagus@gmail.com>
2026-05-04 10:39:37 -0300

    Acomodo Access token publico

--- app/lib/mercadopago.ts
+++ app/lib/mercadopago.ts
@@ -10,7 +10,7 @@
 import { MercadoPagoConfig } from "mercadopago";
 
 const mercadoPagoClient = new MercadoPagoConfig({
-  accessToken: process.env.MP_ACCES_TOKEN!,
+  accessToken: process.env.MP_ACCESS_TOKEN!, options: {timeout :5000}
 });
 
 export default mercadoPagoClient;
