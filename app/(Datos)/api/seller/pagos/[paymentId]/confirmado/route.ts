3d21c54f903126193dd69ac06548330d123f480a
Your Name <cuentauniagus@gmail.com>
2026-05-13 20:25:20 -0300

    Test

--- app/(Datos)/api/seller/pagos/[paymentId]/confirmado/route.ts
+++ app/(Datos)/api/seller/pagos/[paymentId]/confirmado/route.ts
@@ -27,3 +27,7 @@ export async function POST(
 
   return NextResponse.json({ ok: true }, { status: 200 });
 }
+
+export async function GET(){
+  return NextResponse.json({ ok: true }, { status: 200 });
+}
